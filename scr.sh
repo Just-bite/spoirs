20f6be6fca68d0e364b102426f05f128775478a2
Ilya <Ilya@DESKTOP-V9L0ANR>
2026-02-22 20:12:02 +0300

    .

--- scr.sh
+++ scr.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+set -x
+
 git pull
 
 rm -f f.zip
 
-# Run the client script with predefined commands
+echo l_1 
+
 python LAB_1/client.py << EOF
 192.168.84.99
+9090
 download f.zip
 exit
 EOF
 
-# Calculate and display the SHA256 checksum of the downloaded file
 sha256sum f.zip
 rm -f f.zip
 
-# Run the client script with predefined commands
+echo l_2 
+
 python LAB_2/client.py << EOF
 192.168.84.99
 9091
